bea334fdac873abe93981d454f099e846db12f69
Wassup Chicken <74932143+wassup-chicken@users.noreply.github.com>
2024-10-03 23:29:32 -0400

    bubble sort

---
+++ src/BubbleSort.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class BubbleSort {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        int[] intArray = {10, 9, -8, 7, 4, 2, 1};
+
+        for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex >= 0; lastUnsortedIndex--) {
+
+            for (int i = 0; i < lastUnsortedIndex; i++) {
+
+                if (intArray[i] > intArray[i + 1]) {
+                    swap(intArray, i, i + 1);
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(intArray));
+
+    }
+
+    public static void swap(int[] array, int i, int j) {
+        if (i == j) {
+            return;
+        }
+
+        int temp = array[i];
+
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
