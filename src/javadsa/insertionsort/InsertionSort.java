8da78238e90fda71fbb7a8033f530d089ac7d8e8
Wassup Chicken <74932143+wassup-chicken@users.noreply.github.com>
2024-10-06 23:35:56 -0400

    insertion sort

--- src/javadsa/insertionsort/InsertionSort.java
+++ src/javadsa/insertionsort/InsertionSort.java
@@ -4,6 +4,22 @@ import java.util.Arrays;
 
 public class InsertionSort {
 
+    public static void lectureImplementation() {
+
+        int[] intArray = {20, 35, -15, 7, 55, -22};
+
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length; firstUnsortedIndex++) {
+            int newElement = intArray[firstUnsortedIndex];
+
+            int i;
+
+            for(i = firstUnsortedIndex; i > 0 && intArray[i - 1] > newElement; i--) {
+                intArray[i] = intArray[i - 1];
+            }
+
+            intArray[i] = newElement;
+        }
+    }
     public static void main (String[] args) {
         int[] intArray = {20, 35, -15, 7, 55, 1, -22};
 
